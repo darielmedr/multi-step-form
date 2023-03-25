ff649743c619cf062b6426236127f11553bffe4e
darielmedr <darielmedr97@gmail.com>
2023-03-25 16:05:54 +0100

    feat: add shared heading module

---
+++ src/app/shared/components/heading/heading.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeadingComponent {
+  @Input() title = '';
+  @Input() description = '';
+}
---
+++ src/app/shared/components/heading/heading.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HeadingComponent } from './heading.component';
+
+@NgModule({
+  declarations: [HeadingComponent],
+  exports: [HeadingComponent],
+})
+export class HeadingModule {}
