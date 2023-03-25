08bd9962d1e77fb52257c2ff47922705620b0462
darielmedr <darielmedr97@gmail.com>
2023-03-26 00:17:47 +0100

    feat(frequency-selector): set initial value

--- src/app/select-plan/features/frequency-selector/frequency-selector.component.ts
+++ src/app/select-plan/features/frequency-selector/frequency-selector.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { PaymentFrequency } from '../../models/payment-frequency.model';
 import { SwitchSelectorState } from '../../models/switch-selector-state.model';
 
@@ -6,8 +13,11 @@ import { SwitchSelectorState } from '../../models/switch-selector-state.model';
   selector: 'app-frequency-selector',
   templateUrl: './frequency-selector.component.html',
   styleUrls: ['./frequency-selector.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FrequencySelectorComponent {
+export class FrequencySelectorComponent implements OnInit {
+  @Input() initialValue: PaymentFrequency = 'monthly';
+
   @Output() selected: EventEmitter<PaymentFrequency> = new EventEmitter();
 
   public switchState: SwitchSelectorState = 'left';
@@ -20,10 +30,23 @@ export class FrequencySelectorComponent {
     right: 'yearly',
   };
 
+  ngOnInit(): void {
+    this.switchState = this.swapObjKeyValue(this.switchStateMap)[
+      this.initialValue
+    ];
+  }
+
   setState(state: SwitchSelectorState): void {
     this.switchState = state;
 
     const paymentFrequency = this.switchStateMap[state];
     this.selected.emit(paymentFrequency);
+  }
+
+  private swapObjKeyValue<K extends string, V extends string>(
+    obj: Record<K, V>
+  ): Record<V, K> {
+    const reversedEntries = Object.entries(obj).map((v) => v.reverse());
+    return Object.fromEntries(reversedEntries);
   }
 }
