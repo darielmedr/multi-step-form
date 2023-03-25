19a75f0592664be0982cafe093d839d3e321548c
darielmedr <darielmedr97@gmail.com>
2023-03-25 18:17:10 +0100

    feat(plan-selection): set id prop

--- src/app/select-plan/data-access/plan-selection.service.ts
+++ src/app/select-plan/data-access/plan-selection.service.ts
@@ -9,18 +9,21 @@ import { Plan } from '../models/plan.model';
 export class PlanSelectionService {
   private readonly plans: Plan[] = [
     {
+      id: 1,
       name: 'Arcade',
       price: 9,
       logo: './assets/images/icon-arcade.svg',
       discountInfo: '2 months free',
     },
     {
+      id: 2,
       name: 'Advanced',
       price: 12,
       logo: './assets/images/icon-advanced.svg',
       discountInfo: '2 months free',
     },
     {
+      id: 3,
       name: 'Pro',
       price: 15,
       logo: './assets/images/icon-pro.svg',
