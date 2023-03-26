4feb4f3bc41ca8f57fc70b4ffa6c4df8a71817df
darielmedr <darielmedr97@gmail.com>
2023-03-26 17:08:31 +0200

    feat(summary): setup summary page

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AddonService } from '../addons/data-access/addon.service';
+import { Addon } from '../addons/models/addon.model';
+import { PlanSelectionService } from '../select-plan/data-access/plan-selection.service';
+import { PaymentFrequency } from '../select-plan/models/payment-frequency.model';
+import { Plan } from '../select-plan/models/plan.model';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SummaryComponent {}
+export class SummaryComponent implements OnInit {
+  public selectedPlan$: Observable<Plan> = new Observable();
+  public paymentFrequency$: Observable<PaymentFrequency> = new Observable();
+  public selectedAddons$: Observable<Addon[]> = new Observable();
+  public addonsTotalPrice$: Observable<number> = new Observable();
+
+  public title = 'Finishing up';
+  public description = 'Double-check everything looks OK before confirming.';
+
+  constructor(
+    private router: Router,
+    private planSelectionService: PlanSelectionService,
+    private addonService: AddonService
+  ) {}
+
+  ngOnInit(): void {
+    this.selectedPlan$ = this.planSelectionService.getSelectedPlan();
+    this.paymentFrequency$ = this.planSelectionService.getPaymentFrequency();
+    this.selectedAddons$ = this.addonService.getSelectedAddons();
+    this.addonsTotalPrice$ = this.selectedAddons$.pipe(
+      map((addons) =>
+        addons.reduce((totalPrice, addon) => totalPrice + addon.price, 0)
+      )
+    );
+  }
+
+  public trackByFn(_: number, value: Addon): number {
+    return value.id;
+  }
+
+  public changePlan(): void {
+    this.router.navigateByUrl('/select-plan');
+  }
+}
--- src/app/summary/summary.module.ts
+++ src/app/summary/summary.module.ts
@@ -1,10 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeadingModule } from '../shared/components/heading/heading.module';
+import { AddonSummaryModule } from './features/addon-summary/addon-summary.module';
+import { AddonsTotalPriceModule } from './features/addons-total-price/addons-total-price.module';
+import { PlanSummaryModule } from './features/plan-summary/plan-summary.module';
 
 import { SummaryRoutingModule } from './summary-routing.module';
 import { SummaryComponent } from './summary.component';
 
 @NgModule({
   declarations: [SummaryComponent],
-  imports: [SummaryRoutingModule],
+  imports: [
+    CommonModule,
+    SummaryRoutingModule,
+    HeadingModule,
+    AddonSummaryModule,
+    PlanSummaryModule,
+    AddonsTotalPriceModule,
+  ],
 })
 export class SummaryModule {}
