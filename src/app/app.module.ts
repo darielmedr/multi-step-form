5f306b456c5b21969b2a700970df007a33cf490b
darielmedr <darielmedr97@gmail.com>
2023-03-30 00:00:12 +0200

    feat(app): add navigation-actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationActionsModule } from './navigation-actions/navigation-actions.module';
 import { StepperModule } from './stepper/stepper.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StepperModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StepperModule,
+    NavigationActionsModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
